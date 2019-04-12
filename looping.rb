3c8642bfb926db18495b3221b7913e8bfec401d3
brookton <davidbrookton@gmail.com>
2019-04-12 16:19:23 -0600

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def looping
-	#your code here
+  counter = 0
+  loop do
+    counter += 1
+    puts "I have found the Time Machine!"
+    if counter >= 10
+   break
+  end
 end
 
 #call your method here
